dfbf97a0024867eb0de951ec752659002ae1b325
Ela <elamathi@gmail.com>
2018-09-10 01:48:57 +1000

    Adventure works checkin

---
+++ WebApiAdventureWorks/Controllers/AdventureWorks/PersonController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiAdventureWorks.Models.AdventureWorks;
+using WebApiAdventureWorks.Models.AdventureWorks.DTO;
+
+namespace WebApiAdventureWorks.Controllers.AdventureWorks
+{
+    public class PersonController : ApiController
+    {
+        private AdventureWorksContext db = new AdventureWorksContext();
+
+        // GET: api/Person
+        public IEnumerable<PersonDTO> GetPeople()
+        {
+            var People = db.People.Select(t => (new PersonDTO()
+            {
+                FirstName = t.FirstName,
+                MiddleName = t.MiddleName,
+                LastName = t.LastName,
+                EmailAddresses = t.EmailAddresses.Select(x => x.EmailAddress1).ToList()
+            })).ToList();
+
+            return People;
+        }
+
+        // GET: api/Person/5
+        [ResponseType(typeof(Person))]
+        public async Task<IHttpActionResult> GetPerson(int id)
+        {
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // PUT: api/Person/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPerson(int id, Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != person.BusinessEntityID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Person
+        [ResponseType(typeof(Person))]
+        public async Task<IHttpActionResult> PostPerson(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.People.Add(person);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PersonExists(person.BusinessEntityID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = person.BusinessEntityID }, person);
+        }
+
+        // DELETE: api/Person/5
+        [ResponseType(typeof(Person))]
+        public async Task<IHttpActionResult> DeletePerson(int id)
+        {
+            Person person = await db.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            db.People.Remove(person);
+            await db.SaveChangesAsync();
+
+            return Ok(person);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonExists(int id)
+        {
+            return db.People.Count(e => e.BusinessEntityID == id) > 0;
+        }
+    }
+}
